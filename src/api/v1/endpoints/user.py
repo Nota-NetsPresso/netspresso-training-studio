a874755baf0d221e6f9fdc604ef560af561d5690
Only-bottle <ssey0921@gmail.com>
2025-06-12 02:23:18 +0900

    Change the file location

--- src/api/v1/endpoints/user.py
+++ src/api/v1/endpoints/user.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter, Depends
 
-from src.api.deps import api_key_header
+from app.api.deps import api_key_header
+from app.api.v1.schemas.user import UserResponse
+from app.services.user import user_service
 
 router = APIRouter()
 
 
-@router.get("/me")
-def get_user(*, api_key: str = Depends(api_key_header)):
-    pass
+@router.get("/me", response_model=UserResponse)
+def get_user(*, api_key: str = Depends(api_key_header)) -> UserResponse:
+    user = user_service.get_user_info(api_key=api_key)
+
+    return UserResponse(data=user)
