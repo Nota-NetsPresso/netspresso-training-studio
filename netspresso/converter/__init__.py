1d6a32e2e231e4accaa4799cede0513f64b19d11
Only-bottle <bmlee@nota.ai>
2025-06-16 03:38:29 +0900

    Remove unused files and modules for codebase cleanup

--- netspresso/converter/__init__.py
+++
@@ -1,3 +0,0 @@
-from netspresso.converter.v2.converter import ConverterV2
-
-__all__ = ["ConverterV2"]
--- netspresso/inferencer/__init__.py
+++
@@ -1,3 +0,0 @@
-from netspresso.inferencer.inferencer import CustomInferencer, NPInferencer
-
-__all__ = ["NPInferencer", "CustomInferencer"]
