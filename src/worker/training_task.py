efe3aac7eb0c9c1bde7113e7414fcff608be46ce
Only-bottle <bmlee@nota.ai>
2025-06-16 16:17:27 +0900

    Refactor set_dataset method to accept TrainingTask and update dataset attributes

--- src/worker/training_task.py
+++ src/worker/training_task.py
@@ -344,10 +344,12 @@ def train_model(
         logger.info(f"Configuring model and training parameters for task_id: {training_task_id}")
         configure_model_and_training(trainer, training_in, input_model_info)
 
-        trainer.set_dataset(
+        training_task = trainer.set_dataset(
+            training_task=training_task,
             dataset_root_path=training_task.dataset.path,
             dataset_name=training_task.dataset.name,
         )
+        training_task = training_task_repository.update(db=db, training_task=training_task)
 
         # Execute training
         logger.info(f"Starting training with task_id: {training_task_id}")
