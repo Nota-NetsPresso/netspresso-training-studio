726758240c75d5b03db9aaae0fc87575bb16dbe5
Only-bottle <bmlee@nota.ai>
2025-06-16 23:19:29 +0900

    Enhance conversion task handling with existing task check and model creation

--- src/worker/conversion_task.py
+++ src/worker/conversion_task.py
@@ -1,5 +1,8 @@
 from celery import chain
+from sqlalchemy.orm import Session
 
+from src.core.db.session import get_db_session
+from src.enums.model import DataType
 from src.modules.converter.v2.converter import ConverterV2
 from src.worker.celery_app import celery_app
 
@@ -10,39 +13,37 @@ POLLING_INTERVAL = 30  # seconds
 def convert_model(
     self,
     api_key: str,
-    input_model_path: str,
+    input_model_id: str,
+    conversion_task_id: str,
     target_framework: str,
     target_device_name: str,
-    target_data_type: str,
+    target_data_type: str = DataType.FP16,
     target_software_version: str = None,
-    input_layer=None,
     dataset_path: str = None,
-    input_model_id: str = None,
-    conversion_task_id: str = None,
 ):
-    converter = ConverterV2(api_key=api_key)
+    with get_db_session() as db:
+        converter = ConverterV2(api_key=api_key)
 
-    task_id = converter.convert_model(
-        input_model_path=input_model_path,
-        target_framework=target_framework,
-        target_device_name=target_device_name,
-        target_data_type=target_data_type,
-        target_software_version=target_software_version,
-        input_layer=input_layer,
-        dataset_path=dataset_path,
-        input_model_id=input_model_id,
-        conversion_task_id=conversion_task_id,
-        wait_until_done=False,
-    )
+        task_id = converter.convert_model(
+            db=db,
+            input_model_id=input_model_id,
+            conversion_task_id=conversion_task_id,
+            target_framework=target_framework,
+            target_device_name=target_device_name,
+            target_data_type=target_data_type,
+            target_software_version=target_software_version,
+            dataset_path=dataset_path,
+            wait_until_done=False,
+        )
 
-    chain(poll_conversion_status.s(api_key, task_id).set(countdown=POLLING_INTERVAL))()
-    return task_id
+        chain(poll_conversion_status.s(db=db, api_key=api_key, task_id=task_id).set(countdown=POLLING_INTERVAL))()
+        return task_id
 
 
 @celery_app.task
-def poll_conversion_status(api_key: str, task_id: str):
+def poll_conversion_status(db: Session, api_key: str, task_id: str):
     converter = ConverterV2(api_key=api_key)
-    status_updated = converter.update_conversion_task_status(task_id)
+    status_updated = converter.update_conversion_task_status(db=db, task_id=task_id)
 
     if not status_updated:
-        poll_conversion_status.apply_async(args=[api_key, task_id], countdown=POLLING_INTERVAL)
+        poll_conversion_status.apply_async(args=[db, api_key, task_id], countdown=POLLING_INTERVAL)
