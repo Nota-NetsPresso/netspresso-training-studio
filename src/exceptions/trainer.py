e301144d98f0168a1786ab00dd0aa1ce0a98e9e0
Only-bottle <bmlee@nota.ai>
2025-06-17 02:07:08 +0900

    Add MODULE origin to exceptions for consistent error tracking

--- src/exceptions/trainer.py
+++ src/exceptions/trainer.py
@@ -2,6 +2,7 @@ from typing import List
 
 from fastapi import status
 
+from src.enums.exception import Origin
 from src.exceptions.base import AdditionalData, ExceptionBase
 
 
@@ -9,7 +10,7 @@ class NotSupportedTaskException(ExceptionBase):
     def __init__(self, available_tasks: List, task: int):
         message = f"The task supports {available_tasks}. The entered task is {task}."
         super().__init__(
-            data=AdditionalData(origin="pynp"),
+            data=AdditionalData(origin=Origin.MODULE),
             error_code="trainer40001",
             status_code=status.HTTP_400_BAD_REQUEST,
             name=self.__class__.__name__,
@@ -21,7 +22,7 @@ class NotSetDatasetException(ExceptionBase):
     def __init__(self):
         message = "The dataset is not set. Use `set_dataset_config` or `set_dataset_config_with_yaml` to set the dataset configuration."
         super().__init__(
-            data=AdditionalData(origin="pynp"),
+            data=AdditionalData(origin=Origin.MODULE),
             error_code="trainer40002",
             status_code=status.HTTP_400_BAD_REQUEST,
             name=self.__class__.__name__,
@@ -33,7 +34,7 @@ class NotSetModelException(ExceptionBase):
     def __init__(self):
         message = "The model is not set. Use `set_model_config` or `set_model_config_with_yaml` to set the model configuration."
         super().__init__(
-            data=AdditionalData(origin="pynp"),
+            data=AdditionalData(origin=Origin.MODULE),
             error_code="trainer40003",
             status_code=status.HTTP_400_BAD_REQUEST,
             name=self.__class__.__name__,
@@ -45,7 +46,7 @@ class TaskOrYamlPathException(ExceptionBase):
     def __init__(self):
         message = "Either 'task' or 'yaml_path' must be provided, but not both."
         super().__init__(
-            data=AdditionalData(origin="pynp"),
+            data=AdditionalData(origin=Origin.MODULE),
             error_code="trainer40004",
             status_code=status.HTTP_400_BAD_REQUEST,
             name=self.__class__.__name__,
@@ -57,7 +58,7 @@ class NotSupportedModelException(ExceptionBase):
     def __init__(self, available_models: List, model_name: str, task: str):
         message = f"The '{model_name}' model is not supported for the '{task}' task. The available models are {available_models}."
         super().__init__(
-            data=AdditionalData(origin="pynp"),
+            data=AdditionalData(origin=Origin.MODULE),
             error_code="trainer40005",
             status_code=status.HTTP_400_BAD_REQUEST,
             name=self.__class__.__name__,
@@ -69,7 +70,7 @@ class RetrainingFunctionException(ExceptionBase):
     def __init__(self):
         message = "This function is intended for retraining. Please use 'set_model_config' for model setup."
         super().__init__(
-            data=AdditionalData(origin="pynp"),
+            data=AdditionalData(origin=Origin.MODULE),
             error_code="trainer40006",
             status_code=status.HTTP_400_BAD_REQUEST,
             name=self.__class__.__name__,
@@ -81,7 +82,7 @@ class FileNotFoundErrorException(ExceptionBase):
     def __init__(self, relative_path: str):
         message = f"The required file '{relative_path}' does not exist. Please check and make sure it is in the correct location."
         super().__init__(
-            data=AdditionalData(origin="pynp"),
+            data=AdditionalData(origin=Origin.MODULE),
             error_code="trainer40401",
             status_code=status.HTTP_404_NOT_FOUND,
             name=self.__class__.__name__,
@@ -93,7 +94,7 @@ class DirectoryNotFoundException(ExceptionBase):
     def __init__(self, relative_path: str):
         message = f"The required directory '{relative_path}' does not exist. Please check and make sure it is in the correct location."
         super().__init__(
-            data=AdditionalData(origin="pynp"),
+            data=AdditionalData(origin=Origin.MODULE),
             error_code="trainer40402",
             status_code=status.HTTP_404_NOT_FOUND,
             name=self.__class__.__name__,
@@ -105,7 +106,7 @@ class BaseDirectoryNotFoundException(ExceptionBase):
     def __init__(self, base_path: str):
         message = f"The directory '{base_path}' does not exist."
         super().__init__(
-            data=AdditionalData(origin="pynp"),
+            data=AdditionalData(origin=Origin.MODULE),
             error_code="trainer40403",
             status_code=status.HTTP_404_NOT_FOUND,
             name=self.__class__.__name__,
@@ -117,7 +118,7 @@ class FailedTrainingException(ExceptionBase):
     def __init__(self, error_log: str):
         message = "An error occurred during the training process."
         super().__init__(
-            data=AdditionalData(origin="pynp", error_log=error_log),
+            data=AdditionalData(origin=Origin.MODULE, error_log=error_log),
             error_code="trainer50001",
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             name=self.__class__.__name__,
@@ -129,7 +130,7 @@ class NotCompletedTrainingException(ExceptionBase):
     def __init__(self, training_task_id: str):
         message = f"The training task {training_task_id} is not completed."
         super().__init__(
-            data=AdditionalData(origin="pynp"),
+            data=AdditionalData(origin=Origin.MODULE),
             error_code="trainer40007",
             status_code=status.HTTP_400_BAD_REQUEST,
             name=self.__class__.__name__,
